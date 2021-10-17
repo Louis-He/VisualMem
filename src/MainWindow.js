f46b56fcde6866d390344f9a3bf0172e0da3c490
wuxc0116 <wu.xiangcheng@hotmail.com>
2021-10-18 03:03:01 +0800

    Add general UI - Left and Right page seperation

--- src/MainWindow.js
+++ src/MainWindow.js
@@ -76,6 +76,102 @@ export default class MainWindow extends React.Component {
               <div className="App-header"></div>
             </header> */}
             <Container>
+              <div class="wrap">
+                  <div id="left" class="left">
+                        <div id="logoDiv" class="logoDiv">
+                            <p id="logoTitle" class="logoTitle">
+                                <span>Debugger Visualize</span>
+                            </p>
+                        </div>
+                        <div class="menu-title">Menu 1</div>
+                        <div class="menu-item" href="#one" data-toggle="tab">
+                            －functionality 1
+                        </div>
+                        <div class="menu-item" href="#two" data-toggle="tab">
+                            －functionality 2
+                        </div>
+                        <div class="menu-title">Menu 2</div>
+                        <div class="menu-item" href="#three" data-toggle="tab">
+                            －functionality 1
+                        </div>
+                        <div class="menu-item" href="#four" data-toggle="tab">
+                            －functionality 2
+                        </div>
+                  </div>
+                  <div id="right" class="tab-content right">
+                          <div id="one" class="tab-pane active">
+                              <span>
+                              <div className="container_ext">
+                                <Button onClick={renderRequestOpenConfig}>TEST button</Button>
+                                <Button onClick={renderRequestStartGDB}>Start GDB</Button>
+                                <Button onClick={renderRequestStopGDB}>Stop GDB</Button>
+                              </div>
+                              <div>
+                                <label>Select Project Folder
+                                  <span
+                                    htmlFor="files"
+                                    className="btn btn-primary btn-sm"
+                                    style={{ fontSize: "18px", lineHeight: "1" }}>
+                                    <Folder2Open style={{ verticalAlign: 'baseline' }} />
+                                  </span>
+                                  <input id="files"
+                                    webkitdirectory = ""
+                                    style={{ visibility: "hidden", width: "0px" }}
+                                    type="file"
+                                    onClick={(e) => this.selectProjectFolder(e)}
+                                  />
+                                </label>
+                              </div>
+
+                              <div>
+                                <p>Current Selected Folder: {this.props.projectFolder}</p>
+                              </div>
+
+                              <div>
+                                <Form>
+                                  <Form.Group controlId="exampleForm.ControlTextarea1">
+                                    <Form.Label>Command Sent to GDB</Form.Label>
+                                    <Form.Control as="textarea" rows={3}
+                                      value={this.state.GDBCommand}
+                                      onKeyDown={(e) => this.onGDBCommandEnterPress(e)}
+                                      onChange={(e) => this.GDBCommandLineOnChangeHandler(e)} />
+                                  </Form.Group>
+
+                                  <Button variant="primary" onClick={(e) => this.sendGDBCommandButton()}>
+                                    Send
+                                  </Button>
+                                </Form>
+                              </div>
+
+                              <div>
+                                <label>Select Executable
+                                  <span
+                                    htmlFor="files"
+                                    className="btn btn-primary btn-sm"
+                                    style={{ fontSize: "18px", lineHeight: "1" }}>
+                                    <Folder2Open style={{ verticalAlign: 'baseline' }} />
+                                  </span>
+                                  <input id="files"
+                                    style={{ visibility: "hidden", width: "0px" }}
+                                    type="file"
+                                    onClick={(e) => this.selectExecutable(e)}
+                                  />
+                                </label>
+                              </div>
+                              </span>
+                          </div>
+                          <div id="two" class="tab-pane">
+                              <span>
+                                    内容二
+                              </span>
+                          </div>
+                          <div id="three" class="tab-pane">
+                              <span>
+                                    内容三
+                              </span>
+                          </div>
+                  </div>
+        </div>
               <div className="container_ext">
                 <Button onClick={renderRequestOpenConfig}>TEST button</Button>
                 <Button onClick={renderRequestStartGDB}>Start GDB</Button>

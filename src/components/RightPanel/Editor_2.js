6cf764d9af1e7e5e09cad3b7780108e4eee12d45
Xiangcheng Wu <wxc@Xiangchengs-MacBook-Pro.local>
2022-03-30 15:21:22 -0400

    add breakpoint

--- src/components/RightPanel/Editor_2.js
+++ src/components/RightPanel/Editor_2.js
@@ -1,129 +1,189 @@
-import React from "react";
-import AceEditor from "react-ace";
-// import brace from "brace";
+import React from 'react';
+import MonacoEditor from 'react-monaco-editor';
+//import style from "./index.css"
+import "./Editor.css"
 
-//import "brace/mode/typescript";
-//import "brace/theme/tomorrow_night";
-import 'ace-builds/src-noconflict/mode-c_cpp'
-import 'ace-builds/src-noconflict/theme-monokai'
-/*
-function onChange(newValue) {
-  console.log("change", newValue);
-}
-*/
-
-var IntegrationTests = `import unmock, { Service } from 'unmock';
-
-describe("Integration tests with Unmock", () => {
-  // Intercept all HTTP traffic
-  beforeAll(() => unmock.on());
-
-  test("should be awesome for mocking a service ", async () => {
-    const userEmail = await fetchGitHubUserEmail("meeshkan");
-    expect(typeof userEmail).toBe("string");
-  });
-
-  test("should be awesome for modifying fake data", async () => {
-    const github: Service = unmock.services.github;
-    const testUser = "meeshkan";
-    github.state(\`/users/\${testUser}\`, { email: "dev@meeshkan.com" });
-    const userEmail = await fetchGitHubUserEmail(testUser);
-    expect(userEmail).toEqual("dev@meeshkan.com");
-  });
-
-  test("should be awesome for expressive asserts", async () => {
-    const github: Service = unmock.services.github;
-    await codeThatShouldCallGitHubAPI();
-    expect(github).beCalledOnce(); // PROFIT!
-  });
-});
-`;
-
-IntegrationTests = `int main(){
-    printf("hello world");
-    return 0;
-}`
-
-const markers = [
-  {
-    startRow: 3,
-    endRow: 4,
-    startCol: 2,
-    type: "text",
-    className: "test-marker"
+export default class Editor2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: "start coding",
+    }
+    this.editor = null;
+    this.monaco = null;
+    this.decorations = [];
+    this.onChange = this.onChange.bind(this);
+    this.editorDidMount = this.editorDidMount.bind(this);
   }
-];
 
-const annotations = [
-  {
-    row: 3, // must be 0 based
-    column: 4, // must be 0 based
-    text: "error.message", // text to show in tooltip
-    type: "warning"
+  editorDidMount(editor, monaco) {
+    if (!this.editor) {
+      this.editor = editor;
+      this.monaco = monaco;
+      this.editorMousDown();
+      this.onMouseMove();
+    }
+    editor.focus();
   }
-];
 
-function getDelay() {
-  return Math.round(Math.random() * 0) + 1;
-}
+  onChange(newValue, e) {
+    //this.props.parent(this.props.data._id, newValue);
+    this.props.fileUpdatefunc(newValue);
+  }
 
-const Editor = (props, { useDelay } = { useDelay: false }) => {
-  const [textValue, setTextValue] = React.useState("");
-  const { source_code } = props
-  IntegrationTests = source_code;
+  onMouseMove() {
+    this.editor.onMouseMove(e => {
+      if (!this.isJsEditor()) return
+      this.removeFakeBreakPoint()
+      if (e.target.detail && e.target.detail.offsetX && e.target.detail.offsetX >= 0 && e.target.detail.offsetX <= 10) {
+        let line = e.target.position.lineNumber
+        this.addFakeBreakPoint(line)
+      }
+    })
+    this.editor.onMouseLeave(() => {
+      this.removeFakeBreakPoint()
+    })
+    this.editor.onKeyDown(e => {
+      if (e.code === 'Enter') {
+        this.removeFakeBreakPoint()
+      }
+    })
+  }
 
-  React.useEffect(() => {
-    if (useDelay) {
-      let counter = 0;
-      let timeout;
+  trigger(id) {
+    if (!this.editor) return
+    this.editor.trigger('anyString', id)
+  }
 
-      function updateTextAndCounter() {
-        setTextValue(IntegrationTests.slice(0, counter));
-        counter++;
-        if (counter < IntegrationTests.length) {
-          timeout = setTimeout(updateTextAndCounter, getDelay());
+  insertContent(text) {
+    if (this.editor) {
+      let selection = this.editor.getSelection()
+      let range = new this.monaco.Range(selection.startLineNumber, selection.startColumn, selection.endLineNumber, selection.endColumn)
+      let id = {
+        major: 1,
+        minor: 1
+      }
+      let op = {
+        identifier: id,
+        range: range,
+        text: text,
+        forceMoveMarkers: true
+      }
+      this.editor.executeEdits(this.root, [op])
+      this.editor.focus()
+    }
+  }
+
+  editorMousDown() {
+    this.editor.onMouseDown(e => {
+      if (!this.isJsEditor()) {
+          return
+      }
+      if (e.target.detail && e.target.detail.offsetX && e.target.detail.offsetX >= 0 && e.target.detail.offsetX <= 10) {
+        let line = e.target.position.lineNumber
+        if (this.editor.getModel().getLineContent(line).trim() === '') {
+          return
+        }
+        if (!this.hasBreakPoint(line)) {
+          this.addBreakPoint(line)
+        } else {
+          this.removeBreakPoint(line)
+        }
+        if (this.lastPosition) {
+          this.editor.setPosition(this.lastPosition)
+        } else {
+          document.activeElement.blur()
         }
       }
+      if (e.target.type === 6 || e.target.type === 7) {
+        this.lastPosition = this.editor.getPosition()
+      }
+    })
+  }
 
-      timeout = setTimeout(updateTextAndCounter, getDelay());
-      return () => {
-        if (typeof timeout !== "undefined") {
-          clearTimeout(timeout);
-        }
-      };
-    } else {
-      setTextValue(IntegrationTests.slice(0, IntegrationTests.length));
+  isJsEditor() {
+    //return this.editor.getModel().getLanguageIdentifier().language === 'javascript'
+    return true
+  }
+
+  addFakeBreakPoint(line) {
+    if (this.hasBreakPoint(line)) return;
+    let value = {
+      range: new this.monaco.Range(line, 1, line, 1),
+      options: {
+        isWholeLine: true,
+        linesDecorationsClassName: 'breakpoints-fake'
+      }
     }
-  }, [useDelay]);
+    this.decorations = this.editor.deltaDecorations(this.decorations, [value])
+  }
 
-  return (
-    <div>
-      <AceEditor
-        style={{
-            borderRadius: '15px',
-            height: '96vh',
-            width: '100%',
-        }}
-        mode="c_cpp"
-        theme="monokai"
-        width="100%"
-        name="ace-editor"
-        editorProps={{ $blockScrolling: false }}
-        fontSize={18}
-        value={textValue}
-        showGutter={true}
-        setOptions={{
-          enableBasicAutocompletion: false,
-          enableLiveAutocompletion: false,
-          enableSnippets: false,
-          showLineNumbers: true,
-          tabSize: 2
-        }}
-        markers={markers}
-        annotations={annotations}
+  removeFakeBreakPoint() {
+    this.decorations = this.editor.deltaDecorations(this.decorations, [])
+  }
+
+  async addBreakPoint(line) {
+    let model = this.editor.getModel()
+    if (!model) return
+    let value = {
+      range: new this.monaco.Range(line, 1, line, 1),
+      options: {
+        isWholeLine: true,
+        linesDecorationsClassName: 'breakpoints'
+      }
+    }
+    model.deltaDecorations([], [value])
+  }
+
+  async removeBreakPoint(line) {
+    let model = this.editor.getModel()
+    if (!model) return
+    let decorations
+    let ids = []
+    if (line !== undefined) {
+      decorations = this.editor.getLineDecorations(line)
+    } else {
+      decorations = this.editor.getAllDecorations()
+    }
+    for (let decoration of decorations) {
+      if (decoration.options.linesDecorationsClassName === 'breakpoints') {
+        ids.push(decoration.id)
+      }
+    }
+    if (ids && ids.length) {
+      model.deltaDecorations(ids, [])
+    }
+  }
+
+  hasBreakPoint(line) {
+    let decorations = this.editor.getLineDecorations(line)
+    for (let decoration of decorations) {
+      if (decoration.options.linesDecorationsClassName === 'breakpoints') {
+        return true
+      }
+    }
+    return false
+  }
+
+  render() {
+    //const code = this.state.code;
+    const options = {
+      selectOnLineNumbers: true
+    };
+    return (
+      <MonacoEditor
+        className="monacoEditorWrapper"
+        height="98vh"
+        width= "100%"
+        language="python"
+        theme="vs-dark"
+        value={this.props.fileData}
+        options={options}
+        onChange={this.onChange}
+        editorDidMount={this.editorDidMount}
+        borderRadius = "15px"
+        fontSize = "20px"
       />
-    </div>
-  );
-};
-
-export default Editor;
+    );
+  }
+}

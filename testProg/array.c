01e80ee3c9d751e8307f55901ba013d3a45948a8
Qihan <qihan6@qihan.ca>
2022-04-04 22:05:05 -0400

    Added some more c source code tests

--- testProg/array.c
+++ testProg/array.c
@@ -7,19 +7,5 @@ int main () {
 
     int array_1[] = {11, 22, 33, 44, 55};
 
-    int array_3[2][4] = { {10, 11, 12, 13},
-                          {14, 15, 16, 17}};
-    
-    int *array_2 = NULL;
-
-	array_2 = (int*) malloc (4*sizeof(int));
-
-    array_2[0] = 111;
-    array_2[1] = 222;
-    array_2[2] = 333;
-    array_2[3] = 444;
-
-	free(array_2);
-
     return 0;
 }
